f73fd68aa8a9f397ba5ed0235175374d6769e1e6
rafaellarg <rafaellarg@al.insper.edu.br>
2023-11-22 18:33:35 -0300

    alteracao

--- classes.py
+++ classes.py
@@ -1,14 +1,25 @@
 import pygame
 
 
+largura_pessoa = 225
+altura_pessoa = 245
+
+aceleracao = 10
+
 class Pessoa(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-
-        self.image = pygame.image.load("img/pessoa.jpg").convert_alpha()
+        self.image = pygame.image.load("img/pessoa.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (largura_pessoa, altura_pessoa))
         self.rect = self.image.get_rect()
+        self.rect.x = 380
+        self.rect.y = 290
         print(self.rect)
 
-    def update():
-        pass
+    def update(self):
+        if self.rect.x > 830:
+            self.rect.x = 830
+
+        elif self.rect.x < -52:
+            self.rect.x = -52
--- jogo.py
+++ jogo.py
@@ -1,23 +1,45 @@
 import pygame
 from pygame.locals import *
+from classes import Pessoa
 
-largura = 800
+largura = 1000
 altura = 600
+
+aceleracao = 30
 
 pygame.init()
 
 tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Catch the fruits")
 
-fundo_tela = pygame.image.load('img/tela_fundo.jpeg')
+fundo_tela = pygame.image.load('img/tela_fundo.jpg')
 fundo_tela = pygame.transform.scale(fundo_tela, (largura,altura))
-pessoa = pygame.image.load('img/pessoa.jpg').convert_alpha()
 
 
+grupo_pessoa = pygame.sprite.Group()
+pessoa = Pessoa()
+grupo_pessoa.add(pessoa)
+
+relogio = pygame.time.Clock()
+
+# Criando o loop principal do jogo
 while True:
+    # Caso o evento seja do tipo QUIT a janela fecha
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                pessoa.rect.x += aceleracao
+            if event.key == pygame.K_LEFT:
+                pessoa.rect.x -= aceleracao
+
+    # Adicionando a imagem no fundo
     tela.blit(fundo_tela, (0,0))
+
+    grupo_pessoa.update()
+    grupo_pessoa.draw(tela)
+
+    relogio.tick(30)
 
     pygame.display.update()
